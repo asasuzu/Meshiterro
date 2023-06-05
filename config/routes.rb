4b3c6469d5f69c22158db857eee2bf35263b66ba
asasuzu <133328222+asasuzu@users.noreply.github.com>
2023-06-05 15:51:50 +0000

    capter12

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
   
-  resources :post_images, only:[:new, :index, :show]
+  resources :post_images, only:[:new, :create, :index, :show]
   #上の記述で以下のルーティングが生成される。 
   # get 'post_images/new'
   # get 'post_images/index'
