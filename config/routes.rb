d32bbd24835b06ef27991a5583ada35f8bb62aa2
asasuzu <133328222+asasuzu@users.noreply.github.com>
2023-06-06 08:37:31 +0000

    capter15

--- config/routes.rb
+++ config/routes.rb
@@ -3,15 +3,10 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
   
-  resources :post_images, only:[:new, :create, :index, :show]
-  #上の記述で以下のルーティングが生成される。 
-  # get 'post_images/new'
-  # get 'post_images/index'
-  # get 'post_images/show'
-  
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
-get 'homes/about' => 'homes#about' , as: 'about'
+  resources :post_images, only:[:new, :create, :index, :show, :destroy]
+  resources :users, only:[:show, :edit]
+ 
+  get 'homes/about' => 'homes#about' , as: 'about'
 
 end
 
