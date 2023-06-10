79bcfa4a8955913aa198c2b91f459b0dd4a76147
asasuzu <133328222+asasuzu@users.noreply.github.com>
2023-06-10 07:30:02 +0000

    capter19

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
 
   
   resources :post_images, only:[:new, :create, :index, :show, :destroy] do
+    resource :favorites, only: [:create, :destroy]
     resources :post_comments, only: [:create, :destroy]
     # post_image_post_comments POST   /post_images/:post_image_id/post_comments(.:format)     post_comments#create
   
